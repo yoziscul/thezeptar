12ef2cbfc905df6fd1fddbd3b26de8d2b9b8529e
Zeptar Hello <thezeptar@gmail.com>
2023-11-19 05:45:03 +0000

    feat(pages): New about section

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import serviceWorker from 'astrojs-service-worker';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 
 export default defineConfig({
     site: 'https://zeptar.js.cool',
     output: 'server',
-    integrations: [serviceWorker(), react(), tailwind(), sitemap(), compress()],
+    integrations: [serviceWorker(), sitemap(), compress()],
     adapter: vercel(),
     compressHTML: true,
 });
--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-    content: ['./src/{components,pages}/*.{astro,jsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
